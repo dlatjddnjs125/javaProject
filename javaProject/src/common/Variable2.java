0365b47c5fa55a26a921928c0de337f39eef35f4
admin <admin@YD01-03>
2021-02-18 14:02:46 +0900

    test1

--- javaProject/src/common/Variable2.java
+++ javaProject/src/common/Variable2.java
@@ -3,11 +3,11 @@ package common;
 public class Variable2 {
 	public static void main(String[] args) {
 		int a, b;
-		a = 100;
+		a = 1010;
 		b = 200;
 		int result;
-		result = a + b;
-		System.out.println("결과값은 " +result + "입니다.");
+		result = a * b;
+		System.out.println("결과값은 " + result + "입니다.");
 		
 	}
 }
