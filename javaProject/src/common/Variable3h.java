3a927a4000560b704461d0f812973a0f53f14ca6
설기시루 <@59.25.137.153>
2021-02-18 23:01:02 +0900

    1

---
+++ javaProject/src/common/Variable3h.java
@@ -0,0 +1,16 @@
+package common;
+
+public class Variable3h {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b;
+		a = 10;
+		b = 20;
+		int result = b / a;
+		System.out.println("result => " + result);
+	
+
+	}
+
+}
